34fcd89568ff917003da35ced50f5025cfc961a2
Nishant <nishantkarekar98@gmail.com>
2024-12-25 13:30:39 +0530

    "add login register form"

--- src/componets/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({ element }) {
-  const cartItems = useSelector((state) => state.cart.cart);
-  return cartItems.length > 0 ? element : <Navigate to="/" />;
-}
-
-export default ProtectedRoute;
